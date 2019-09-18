15ac8d6f7d8c513a3e2b47f5b493233030a194c3
Rachel Givens <rachelgivens@me.com>
2019-09-18 16:43:28 -0500

    added base footer

---
+++ client/src/Component/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+    <footer className="page-footer">
+        <div className="container-fluid text-center">
+            <p>Rachel Givens</p>
+        </div>
+    </footer>
+);
+
+export default Footer;
